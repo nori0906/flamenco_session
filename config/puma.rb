ca4f6bbbe29b992b659394f224cc147d20e4be07
nunokawa_takanori <job.football.0801@gmail.com>
2022-10-26 22:15:53 +0900

    Add: renderへデプロイ設定

--- config/puma.rb
+++ config/puma.rb
@@ -30,14 +30,18 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+# 2022/10/26 Renderでデプロイする際に変更 renderのドキュメント（https://render.com/docs/deploy-rails#go-production-ready）
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+
+# 2022/10/26 Renderでデプロイする際に変更 renderのドキュメント（https://render.com/docs/deploy-rails#go-production-ready）
+preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
