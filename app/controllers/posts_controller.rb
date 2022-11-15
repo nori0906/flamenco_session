c528f3f509be9b9584e1b040dd40ea3220f2a9e5
nunokawa_takanori <job.football.0801@gmail.com>
2022-11-15 14:17:49 +0900

    Add: 録音表示、再生機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,48 @@
 class PostsController < ApplicationController
   
   def index
-    @posts = Post.all
-    @post = Post.find(40)
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
     @post = Post.new
+    @id = params[:ref_id]
+    if @id
+      @ref_post = Post.find(@id)
+    end
+    if @ref_post.present? && @ref_post.collab_src #左から実行。present?で値の有無を確認し、エラーを防ぐ
+      @root_post = Post.find(@ref_post.collab_src)
+    end
   end
-
+  
   def create
     voice_data = voice_params[:voice]
     
     File.open("tempfile.webm", "wb") do |f|
       f.write(Base64.decode64(voice_data))
     end
-
-    post = Post.new(title: "")
+    
+    if params[:ref_id]
+      @ref_post = Post.find(params[:ref_id])
+      post = Post.new(title: "", collab_src: @ref_post.id)
+    else
+      post = Post.new(title: "")
+    end
     post.voice.attach(io: File.open("tempfile.webm"), filename: "newfile.webm")
     if post.save
       File.delete("tempfile.webm")
       render json: { id: post.id }
     end
   end
-
+  
   def show
     @post = Post.find(params[:id])
-    redirect_to edit_post_path(params[:id])
+    if @post.collab_src
+      @ref_post = Post.find_by(id: @post.collab_src)
+    end
+    if @ref_post.present? && @ref_post.collab_src
+      @root_post = Post.find(@ref_post.collab_src)
+    end
   end
 
   def edit
--- app/javascript/packs/create-audio.js
+++ app/javascript/packs/create-audio.js
@@ -1,9 +1,10 @@
-main()
+createAudio()
 
-async function main () {
+async function createAudio () {
   const buttonStart = document.querySelector('#buttonStart')
   const buttonStop = document.querySelector('#buttonStop')
   const player = document.querySelector('#player')
+  const refRecord = document.querySelector('#refRecord')
   
   const stream = await navigator.mediaDevices.getUserMedia({ // <1>
     video: false,
@@ -19,9 +20,14 @@ async function main () {
   })
   
   buttonStart.addEventListener('click', () => {
-    mediaRecorder.start() // <4>
-    buttonStart.setAttribute('disabled', '')
-    buttonStop.removeAttribute('disabled')
+    if(refRecord){
+      console.log(refRecord);
+      // 録音開始時と同時に元音声を再生させる
+      refRecord.play()
+    }
+      mediaRecorder.start() // <4>
+      buttonStart.setAttribute('disabled', '')
+      buttonStop.removeAttribute('disabled')
   })
   
   buttonStop.addEventListener('click', () => {
@@ -42,9 +48,13 @@ async function main () {
       base64 = base64.split(',')[1];
       console.log(base64);
       
+
+      // クエリパラメーター取得
+      const queryParam = window.location.search
+
       axios({
         method: 'post',
-        url: 'http://localhost:3000/posts',
+        url: 'http://localhost:3000/posts' + queryParam,
         data: {
           voice: base64
         },
@@ -55,7 +65,7 @@ async function main () {
       })
       // 追加
       .then(function (response) {
-        console.log(window.location.href = '/posts/' + response.data.id);
+        console.log(window.location.href = '/posts/' + response.data.id + '/edit');
       })
       .catch(function (error) {
         console.log("error");
