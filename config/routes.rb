bf156bf49180a8b3d4d789832c2b0133baca95b2
nunokawa_takanori <job.football.0801@gmail.com>
2023-01-17 00:32:04 +0900

    Add: トップページのレイアウト大枠を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'user_sessions#new'
+  root to: 'static_pages#top'
   
   get 'login', to: 'user_sessions#new'
   post 'login', to: "user_sessions#create"
