63f74eeab803b0b3cbe7329002ba1f949b3afcab
nunokawa_takanori <job.football.0801@gmail.com>
2023-04-06 11:57:14 +0900

    Add: ユーザープロフィール機能作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,23 +1,27 @@
 class ProfilesController < ApplicationController
-  skip_before_action :require_login
-  skip_before_action :set_user, only %i[edit update]
+  before_action :set_user, only: %i[show edit update]
 
   def show; end
 
   def edit; end
 
   def update
+    if @user.update(user_params)
+      redirect_to profile_path
+    else
+      render :edit
+    end
 
   end
 
   private
 
   def set_user
-
+    @user = User.find(current_user.id)
   end
 
   def user_params
-
+    params.require(:user).permit(:email, :name, :avatar, :avatar_cache)
   end
 
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -20,6 +20,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+  def default_url
+    'flamenco_icon.jpeg'
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
@@ -35,9 +38,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
