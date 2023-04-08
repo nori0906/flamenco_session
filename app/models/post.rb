3af4bad4399775af43d3f0d1b09134fcb539d0ff
nunokawa_takanori <job.football.0801@gmail.com>
2023-04-08 12:43:59 +0900

    Add: 録音時のノイズ対策の制約を追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ApplicationRecord
   has_one_attached :voice
 
   validates :title, presence: true, on: :update
-  validates :body, length: { maximum: 100 } 
-  validates :collab_src, numericality: true, if: :collab_src? 
+  validates :body, length: { maximum: 100 }
+  validates :collab_src, numericality: true, if: :collab_src?
 
 
   enum status: { draft: 0, published: 1 }
