217a00d4658ab77f5512d5d36a8045fa1ad5c3ba
nunokawa_takanori <job.football.0801@gmail.com>
2023-05-03 01:29:22 +0900

    Add: WebAudioAPIでプログレスバーの雛形を作成

--- app/javascript/packs/tutorial-audio.js
+++ app/javascript/packs/tutorial-audio.js
@@ -8,6 +8,17 @@ document.addEventListener('DOMContentLoaded', function () {
   const audioPlayer = document.getElementById('audio-player')
   const recordAudioPlayer = document.getElementById('record-audio-player')
 
+  // コンローラー追加
+  const progressContainer = document.getElementById('progress-container');
+  const progressBar = document.getElementById('progress-bar');
+  const playhead = document.getElementById('playhead');
+  const playbackTime = document.getElementById('playback-time');
+  let source;
+  let startTime;
+
+
+
+  // WebAudioAPI
   const audioContext = new (window.AudioContext || window.webkitAudioContext)();
   let mediaRecorder;
   let recordedChunks = [];
@@ -22,6 +33,8 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   };
 
+
+  // 録音処理
   async function startRecording() {
     recordButton.disabled = true;
     stopButton.disabled = false;
@@ -37,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
     
     mediaRecorder.addEventListener('stop', async () => {
       const blob = new Blob(recordedChunks);
-      recordAudioPlayer.src = URL.createObjectURL(blob);
+      // recordAudioPlayer.src = URL.createObjectURL(blob);
 
       const arrayBuffer = await new Promise((resolve) => {
         const reader = new FileReader();
@@ -53,8 +66,7 @@ document.addEventListener('DOMContentLoaded', function () {
       playRecordingButton.disabled = false;
     });
   }
-  
-  
+
   async function stopRecording() {
     if (mediaRecorder && mediaRecorder.state !== 'inactive') {
       mediaRecorder.stop();
@@ -62,36 +74,97 @@ document.addEventListener('DOMContentLoaded', function () {
       recordButton.disabled = false;
     }
   }
-  
+
   async function playRecording() {
     if (buffer) {
-      const bufferSource = audioContext.createBufferSource();
-      bufferSource.buffer = buffer;
-      bufferSource.connect(audioContext.destination);
-      bufferSource.start(0);
-      stopRecordingButton.disabled = false;
-      playRecordingButton.disabled = true;
+      startTime = audioContext.currentTime;
 
-      stopRecordingButton.addEventListener('click', () => {
-        bufferSource.stop();
-        stopRecordingButton.disabled = true;
-        playRecordingButton.disabled = false;
-      })
+
+      source = audioContext.createBufferSource();
+      source.buffer = buffer;
+      source.connect(audioContext.destination);
+      source.start(0);
+      playRecordingButton.disabled = true;
+      stopRecordingButton.disabled = false;
+
+      updateProgress();
     }
   }
+
+  async function stopPlayRecording() {
+    if (source) {
+      source.stop();
+      source = null;
+      playRecordingButton.disabled = false;
+      stopRecordingButton.disabled = true;
+    }
+  }
+
+
+  // コントローラー関連
+  function updateProgress() {
+    if (source && buffer) {
+      const elapsedTime = audioContext.currentTime - startTime;
+      const progressRatio = elapsedTime / buffer.duration;
+      progressBar.style.width = progressRatio * 100 + '%';
   
+      // 追加: 再生位置のマーカーと再生時間を更新
+      playhead.style.left = progressRatio * 100 + '%';
+      const minutes = Math.floor(elapsedTime / 60);
+      const seconds = Math.floor(elapsedTime % 60);
+      playbackTime.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  
+      if (elapsedTime >= buffer.duration) {
+        playRecordingButton.disabled = false;
+        stopRecordingButton.disabled = true;
+      } else {
+        requestAnimationFrame(updateProgress);
+      }
+    }
+  }
+
+  progressContainer.addEventListener('click', (event) => {
+    if (source && buffer) {
+      const clickX = event.clientX;
+      const containerStartX = progressContainer.getBoundingClientRect().left;
+      const containerWidth = progressContainer.clientWidth;
+
+      const clickPositionRatio = (clickX - containerStartX) / containerWidth;
+      const newTime = clickPositionRatio * buffer.duration;
+
+      if (!playRecordingButton.disabled) {
+        source.stop();
+      }
+
+      startTime = audioContext.currentTime - newTime;
+      source = audioContext.createBufferSource();
+      source.buffer = buffer;
+      source.connect(audioContext.destination);
+      source.start(0, newTime);
+
+      playRecordingButton.disabled = true;
+      stopRecordingButton.disabled = false;
+
+      updateProgress();
+    }
+  });
+
+
+
+  // 元となる音源の音声データの再生処理
   async function playAudio() {
     audioPlayer.play();
     playAudioButton.disabled = true;
     stopAudioButton.disabled = false;
   }
-  
+
   async function stopAudio() {
     audioPlayer.pause();
     playAudioButton.disabled = false;
     stopAudioButton.disabled = true;
   }
-  
+
+
   // エラーハンドリングをまとめる
   function withErrorHandling(fn) {
     return async function (...args) {
@@ -102,14 +175,16 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     };
   }
+
   // withErrorHandlingで各関数をラップしてエラーハンドリングおこなう
   function addEventListeners() {
     recordButton.addEventListener('click', withErrorHandling(startRecording));
     stopButton.addEventListener('click', withErrorHandling(stopRecording));
     playRecordingButton.addEventListener('click', withErrorHandling(playRecording));
+    stopRecordingButton.addEventListener('click', withErrorHandling(stopPlayRecording));
+
     playAudioButton.addEventListener('click', withErrorHandling(playAudio));
     stopAudioButton.addEventListener('click', withErrorHandling(stopAudio));
   }
-
   addEventListeners();
 });
