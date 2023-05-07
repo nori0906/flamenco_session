10eb1792ef00e151e1eef5ca1b498e57ed417619
nunokawa_takanori <job.football.0801@gmail.com>
2023-05-07 16:51:14 +0900

    Add: チュートリアルの既存音声ファイルと録音ファイルの再生処置/音声コントローラーの作成

--- app/javascript/packs/tutorial-recording.js
+++ app/javascript/packs/tutorial-recording.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const playAudioButton = document.getElementById('play-audio-button');
-  const stopAudioButton = document.getElementById('stop-audio-button');
-
+  const recordButton = document.getElementById('record-button');
+  const stopButton = document.getElementById('stop-button');
+  const playRecordingButton = document.getElementById('play-recording-button');
+  const stopRecordingButton = document.getElementById('stop-recording-button');
   
   
   // コンローラー追加
-  const playbackTime = document.getElementById('playback-audio-time');
-  const slider = document.getElementById('audio-slider');
+  const playbackTime = document.getElementById('playback-recording-time');
+  const slider = document.getElementById('recording-slider');
   let source;
   let startTime;
   let resumeTime = 0; // 一時停止時間を保存する変数
@@ -14,12 +15,76 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // WebAudioAPI
   const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  let mediaRecorder;
+  let recordedChunks = [];
   let buffer;
 
+  const constraints = {
+    "video": false,
+    "audio": {
+      "echoCancellation": false,
+      "autoGainControl": false,
+      "noiseSuppression": false
+    }
+  };
 
 
+  // 録音処理
+  async function startRecording() {
+    // 追加: bufferとsourceをリセット
+    buffer = null;
+    source = null;
 
-  async function playAudio() {
+    // 追加: スライダーの値と再生時間をリセット
+    slider.value = 0;
+    playbackTime.textContent = '0:00';
+    resumeTime = 0;
+
+    recordButton.disabled = true;
+    stopButton.disabled = false;
+    playRecordingButton.disabled = true;
+
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    mediaRecorder = new MediaRecorder(stream); // 「mediaRecorder」とは？: 録音機能とそのデータを取得ができる
+    mediaRecorder.start();
+
+    mediaRecorder.addEventListener('dataavailable', (event) => {
+      recordedChunks.push(event.data);
+    });
+
+    mediaRecorder.addEventListener('stop', async () => {
+      const blob = new Blob(recordedChunks);
+
+      const arrayBuffer = await new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsArrayBuffer(blob);
+        reader.onloadend = () => {
+          resolve(reader.result);
+        };
+      });
+      
+      const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+      recordedChunks = [];
+      buffer = audioBuffer;
+      playRecordingButton.disabled = false;
+    });
+  }
+
+  async function stopRecording() {
+    if (mediaRecorder && mediaRecorder.state !== 'inactive') {
+      mediaRecorder.stop();
+      stopButton.disabled = true;
+      recordButton.disabled = false;
+    }
+
+     // bufferがnullでないことを確認
+    if (buffer) {
+      // スライダーの最大値を更新
+      slider.max = buffer.duration;
+    }
+  }
+
+  async function playRecording() {
     if (buffer) {
       startTime = audioContext.currentTime - resumeTime; // resumeTimeを考慮する
 
@@ -32,15 +97,15 @@ document.addEventListener('DOMContentLoaded', function () {
       source.buffer = buffer;
       source.connect(audioContext.destination);
       source.start(0, resumeTime);
-      playAudioButton.disabled = true;
-      stopAudioButton.disabled = false;
+      playRecordingButton.disabled = true;
+      stopRecordingButton.disabled = false;
 
       // 再生を開始する前にupdateProgress関数を呼び出す
       updateProgress();
     }
   }
 
-  async function stopAudio() {
+  async function stopPlayRecording() {
     if (source) {
       // 一時停止時間を保存
       resumeTime = audioContext.currentTime - startTime;
@@ -51,15 +116,14 @@ document.addEventListener('DOMContentLoaded', function () {
       source = null;
 
       // ボタンの状態を更新
-      playAudioButton.disabled = false;
-      stopAudioButton.disabled = true;
+      playRecordingButton.disabled = false;
+      stopRecordingButton.disabled = true;
     }
   }
 
 
-
-
   // 録音コントローラー
+  // 再生時間をリセットし、再生ボタンをクリックしたときに音声が初めから再生
   function resetPlayback() {
     slider.value = 0;
     playbackTime.textContent = '0:00';
@@ -77,12 +141,12 @@ document.addEventListener('DOMContentLoaded', function () {
       playbackTime.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
 
       if (elapsedTime >= buffer.duration) {
-        playAudioButton.disabled = false;
-        stopAudioButton.disabled = true;
-        
+        playRecordingButton.disabled = false;
+        stopRecordingButton.disabled = true;
+
         // 再生が終了した場合、再生時間をリセット
         resetPlayback();
-        resumeTime = 0; // 追加: 再生が最後まで終了した場合にresumeTimeをリセット
+        resumeTime = 0; // 再生が最後まで終了した場合にresumeTimeをリセット
         source = null; // 再生が最後まで終了した場合にsourceをリセット
       } else {
         requestAnimationFrame(updateProgress);
@@ -122,30 +186,14 @@ document.addEventListener('DOMContentLoaded', function () {
   
       // ボタンの状態を更新
       source.onended = () => {
-        playAudioButton.disabled = false;
-        stopAudioButton.disabled = true;
+        playRecordingButton.disabled = false;
+        stopRecordingButton.disabled = true;
       };
   
       updateProgress();
     }
   });
-  
 
-
-
-
-  // 既存音声ファイルを読み込み
-  async function fetchAudio(url) {
-    const response = await fetch(url);
-    const arrayBuffer = await response.arrayBuffer();
-    // 音声ファイルのデータがデコードされ、WebaudioAPIで使用できるようになる
-    buffer = await audioContext.decodeAudioData(arrayBuffer);
-    
-    // 再生時間を更新するためのスライダーの最大値を設定
-    slider.max = buffer.duration;
-  }
-  // ページの読み込み時に音声ファイルをフェッチ
-  fetchAudio('/test.mp3');
 
 
   // エラーハンドリングをまとめる
@@ -164,10 +212,10 @@ document.addEventListener('DOMContentLoaded', function () {
   //イベントリスナー
   // withErrorHandlingで各関数をラップしてエラーハンドリングおこなう
   function addEventListeners() {
-    // recordButton.addEventListener('click', () => withErrorHandling(startRecording)());
-    // stopButton.addEventListener('click', () => withErrorHandling(stopRecording)());
-    playAudioButton.addEventListener('click', () => withErrorHandling(playAudio)());
-    stopAudioButton.addEventListener('click', () => withErrorHandling(stopAudio)());
+    recordButton.addEventListener('click', () => withErrorHandling(startRecording)());
+    stopButton.addEventListener('click', () => withErrorHandling(stopRecording)());
+    playRecordingButton.addEventListener('click', () => withErrorHandling(playRecording)());
+    stopRecordingButton.addEventListener('click', () => withErrorHandling(stopPlayRecording)());
   }
   addEventListeners();
 });
