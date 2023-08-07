f4bf89c4d72864bde1cdd469866677a7a35f7796
nunokawa_takanori <job.football.0801@gmail.com>
2023-08-07 13:37:09 +0900

    Add: プロフィール：公開設定ごとの投稿一覧を表示 #43

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,26 @@
 class ProfilesController < ApplicationController
   before_action :set_user, only: %i[show edit update]
 
-  def show; end
+  def show
+    # ブラウザによって対応しているMIMEが異なるため、ブラウザに対応する投稿一覧を取得 要見直し23/8/7
+    browser = request.browser
+    if browser == "Chrome" || browser == "Edge"
+      user_posts = @user.posts.where(ext_type: "webm")
+    elsif browser == "Safari"
+      user_posts = @user.posts.where(ext_type: "m4a")
+    end
+    
+    respond_to do |format|
+      format.html {
+        @published_posts = user_posts.published.order(created_at: :desc)
+      }
+      format.js {
+        @published_posts = user_posts.published.order(created_at: :desc) if params[:type] == "published"
+        @unpublished_posts = user_posts.unpublished.order(created_at: :desc) if params[:type] == "unpublished"
+        @draft_posts = user_posts.draft.order(created_at: :desc) if params[:type] == "draft"
+      }
+    end
+  end
 
   def edit; end
 
