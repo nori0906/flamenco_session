9c18087a88d7150f505afd6e8c08ce9df5dd3e7d
nunokawa_takanori <job.football.0801@gmail.com>
2023-11-09 02:58:25 +0900

    Add: webpack/ローダーの設定を追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,27 @@
 const { environment } = require('@rails/webpacker')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+// 環境に応じてローダーを切り替える
+const styleLoader = process.env.RAILS_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader';
+
+environment.loaders.append('sass', {
+  test: /\.scss$/,
+  use: [
+    styleLoader, // 開発時は 'style-loader', 本番時は MiniCssExtractPlugin.loader
+    'css-loader',
+    'sass-loader'
+  ]
+})
+
+
+// 本番環境の場合のみプラグインを追加
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.append(
+    'MiniCssExtract',
+    new MiniCssExtractPlugin({
+      filename: '[name]-[contenthash].css'
+    })
+  )
+}
 
 module.exports = environment
