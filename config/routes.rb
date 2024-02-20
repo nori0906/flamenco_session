88b5786c97a1bae448c9494a57a114cee65879b2
nunokawa_takanori <job.football.0801@gmail.com>
2024-02-21 07:11:57 +0900

    Add: コメントアウトを修正 #102

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   resource :profile, only: %i[show edit update]
   resources :password_resets, only: %i[new create edit update]
 
-  # letter_opener_webにアクセスするためのルーテクング設定
+  # letter_opener_webにアクセスするためのルーティング設定
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
