77ea2996cda73f83692557358eebc023527bf51c
nunokawa_takanori <job.football.0801@gmail.com>
2024-02-21 13:52:07 +0900

    Fix: デプロイエラー/ seedファイルに関するビルドスクリプトを修正 #102

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,7 +6,7 @@ bundle install
 RAILS_ENV=production bundle exec rails assets:precompile
 RAILS_ENV=production bundle exec rails assets:clean
 RAILS_ENV=production bundle exec rails db:migrate
-RAILS_ENV=production bundle exec rails db:migrate:reset
+RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 RAILS_ENV=production bundle exec rails db:seed
 
 
