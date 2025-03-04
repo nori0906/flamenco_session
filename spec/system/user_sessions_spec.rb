72d66a849b151c14efee80efbadb7426d01502d6
nunokawa_takanori <job.football.0801@gmail.com>
2025-03-04 17:00:30 +0900

    Add: UserSessions to system spec #130

---
+++ spec/system/user_sessions_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe "UserSessions", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  # ユーザーはログインできる
+  scenario "user can login" do
+    user = User.create(
+      name: "田中",
+      email: "tester@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+    visit root_path
+    within "#rspec-login" do
+      click_on "ログイン"
+    end
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: "password"
+    click_on "ログイン"
+    expect(page).to have_text "投稿一覧"
+    expect(page).to have_text "ログインしました"
+    expect(page).to have_current_path posts_path
+  end
+  
+
+  # ユーザーはログアウトできる
+  scenario "user can logout" do
+    user = User.create(
+      name: "田中",
+      email: "tester@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+    visit root_path
+    within "#rspec-login" do
+      click_on "ログイン"
+    end
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: "password"
+    click_on "ログイン"
+    expect(page).to have_text "投稿一覧"
+    expect(page).to have_text "ログインしました"
+    expect(page).to have_current_path posts_path
+
+    within ".offcanvas-body" do
+      click_on "ログアウト"
+    end
+    save_and_open_page
+    expect(page).to have_text "ログイン"
+    expect(page).to have_text "ログアウトしました"
+    expect(page).to have_current_path login_path
+  end
+  
+end
