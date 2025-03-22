3bd7e39d232817bbb99f80ac560c00c2ae071689
nunokawa_takanori <job.football.0801@gmail.com>
2025-03-22 20:37:15 +0900

    Add: Recording completion test to system spec #130

---
+++ spec/system/recordings_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Recordings", type: :system, js: true do
+  
+  # before(:each) do
+  #   type = page.evaluate_script('typeof navigator.mediaDevices')
+  #   puts "typeof navigator.mediaDevices: #{type}"
+  # end
+
+  # 録音を完了する
+  scenario "user finish recording" do
+    user = User.create(
+      name: "田中",
+      email: "tester@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+
+    visit login_path
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: "password"
+    click_on "ログイン"
+
+    visit new_post_path
+    puts "Current URL: #{page.current_url}"
+    click_on "REC"
+    # FIXME: テスト実行時に一時的に止めてマイク許可を手動で行う
+    # binding.pry
+    expect(page).to have_text "STOP"
+    click_on "STOP"
+    expect(page).to have_text "次へ"
+  end
+end
