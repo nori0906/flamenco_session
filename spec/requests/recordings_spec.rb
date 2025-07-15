e7aef72694fc487551c0ce1216f0a1316cfcedad
nunokawa_takanori <job.football.0801@gmail.com>
2025-07-15 10:50:13 +0900

    Add: return 302 and redirect login_path for request spec #130 #133

--- spec/requests/recordings_spec.rb
+++ spec/requests/recordings_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
+### 初期化 ###
 RSpec.describe "Recordings", type: :request do
   let(:audio_path) { Rails.root.join("spec/fixtures/files/test_silent.webm") }
+  let(:audio_file) { fixture_file_upload(audio_path, 'audio/webm')} # 音声ファイルをアップロード
 
   before do
     FfmpegHelper.generate_silent_webm(path: audio_path.to_s) unless FfmpegHelper.fixture_exists?(audio_path)
@@ -21,10 +23,15 @@ RSpec.describe "Recordings", type: :request do
 
 
   ### テスト ###
-  ## 前提処理
-  xdescribe "ログインを確認" do
+  ## 前提確認
+  xdescribe "認証を確認" do
     it "ログイン済みであること" do
       expect(response.body).to include('ログインしました') # どういうロジックで確認するのが良いかわからなかった
+    end
+
+    it "ログアウト済みであること" do
+      delete logout_path
+      expect(response.body).to include('ログアウトしました') # どういうロジックで確認するのが良いかわからなかった
     end
   end
 
@@ -37,10 +44,8 @@ RSpec.describe "Recordings", type: :request do
 
   ### メインテスト
   ## 正常系
-  xdescribe "return 200" do
-    it "when audio_file" do
-      # リクエスト：`fixture_file_upload`で音声ファイルをアップロード
-      audio_file = fixture_file_upload(audio_path, 'audio/webm')
+  xdescribe "正常な音声データが渡された場合" do
+    it "200ステータスが返る" do
       # データとタイプを設定しサーバーへ送信
       post recordings_path, params: {
         recording: {
@@ -49,23 +54,58 @@ RSpec.describe "Recordings", type: :request do
       }, headers: {
         "CONTENT_TYPE" => "multipart/form-data"
       }
-      # レスポンス：`expect(response).to have_http_status(:ok)`で検証
-      expect(response).to have_http_status(200)
+      # レスポンス
+      expect(response).to have_http_status(:ok)
     end
   end
 
 
   ## 異常系
-  describe "return 422" do
-    it "when voice nil" do
+  xdescribe "音声データがnilで渡された場合" do
+    it "422ステータスを返す" do
       post recordings_path, params: {
         recording: {
           voice: nil
         }
         },headers: {
           "CONTENT_TYPE" => "multipart/form-data"
-      }
+        }
       expect(response).to have_http_status(:unprocessable_entity)
     end
   end
+
+
+  ## 認証系
+  describe "未ログイン時に音声データが渡された場合" do
+    it "302ステータスを返す" do
+      delete logout_path
+      post recordings_path, params: {
+        recording: {
+          voice: audio_file
+        }
+      }, headers: {
+        "CONTENT_TYPE" => "multipart/form-data"
+      }
+      # statusチェック
+      puts "# status結果： #{response.status}"
+      expect(response).to have_http_status(302)
+      # expect(response).to have_http_status(:redirect)
+    end
+
+    it "ログイン画面にリダイレクトされる" do
+      delete logout_path
+      post recordings_path, params: {
+        recording: {
+          voice: audio_file
+        }
+      }, headers: {
+        "CONTENT_TYPE" => "multipart/form-data"
+      }
+      # Location（リダイレクト先）チェック
+      puts "# Location結果： #{response.headers['Location']}"
+      expect(response).to redirect_to(login_path)
+    end
+  end
+
+
 end
